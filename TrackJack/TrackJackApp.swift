def8246c45d28677db47a1640f46fde13cd4f5ae
Camden Bettencourt <115360037+Camden-Bett@users.noreply.github.com>
2025-07-30 21:50:49 -0400

    Initial Commit

---
+++ TrackJack/TrackJackApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrackJackApp.swift
+//  TrackJack
+//
+//  Created by Camden Bettencourt on 7/30/25.
+//
+
+import SwiftUI
+
+@main
+struct TrackJackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
